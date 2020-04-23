33695ed2b5b7063fc733d0dc0ec0162d8f74c2e0
Robi Gunawan <inirobi33@gmail.com>
2020-04-23 09:41:28 +0700

    check sound brow

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
 <div class="page-title">
   <div class="title_left">
     <h3>Invoice <small>Some examples to get you started</small></h3>
+    <h3>Invoice <small>Some examples to get you started</small></h3>
   </div>
 </div>
 @endsection
