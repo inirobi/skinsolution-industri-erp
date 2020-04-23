bdb09028edc2fba2249085bdcaef7a01381e7fb8
Robi Gunawan <inirobi33@gmail.com>
2020-04-23 13:08:47 +0700

    fix confilict di view master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,7 +15,6 @@
     <!-- Font Awesome -->
     <link href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
     <!-- NProgress -->
-<<<<<<< HEAD
     <link href="assets/vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
     <link href="assets/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
@@ -25,9 +24,7 @@
     <link href="assets/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
-=======
     <link href="{{ asset('assets/vendors/nprogress/nprogress.css')}}" rel="stylesheet">
->>>>>>> IniRobi
     
     <!-- Custom styling plus plugins -->
     <link href="{{ asset('assets/build/css/custom.min.css')}}" rel="stylesheet">
