9648b2123d8a0cb44b5a3c5c0555a11b7be68361
Robi Gunawan <inirobi33@gmail.com>
2020-05-07 11:06:51 +0700

    add migration add column tanggal on incomes and expenses table

--- app/PurchaseDetail.php
+++ app/PurchaseDetail.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class PurchaseDetail extends Model
 {
     protected $table = 'purchase_details';
-<<<<<<< HEAD
     public $incrementing = false;
   protected $fillable = ['id','purchase_id','material_id','quantity','expired_date', 'batch_num','analis_num'];
-}
-=======
-    protected $guarded  = ['id'];
+
+    // protected $guarded  = ['id'];
 
     public function material()
     {
@@ -25,4 +23,3 @@ class PurchaseDetail extends Model
     }    
 
 }
->>>>>>> 8c05e0ed52552ec121a033663333449f908092ef
